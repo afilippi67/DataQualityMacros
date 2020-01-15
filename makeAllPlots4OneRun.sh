b52801b1a39bbb39e7e10b290ea9c53b6da1e736
Alessandra Filippi <filippi@to.infn.it>
2020-01-15 13:35:42 +0100

    compliance to new histogram namings, 2015-2016 geometry setup (6 layers)

--- makeAllPlots4OneRun.sh
+++ makeAllPlots4OneRun.sh
@@ -3,13 +3,6 @@
 export FILE=$1
 export RUN=$2
 export TYPE=run_$RUN
-#root -b -l -q plotResidualsOrder.cxx\(\"$FILE\"\)
-#root -b -l -q plotResidualsOrderGBLWithErrors.cxx\(\"$FILE\"\)
-#root -b -l -q plotResidualsOrderScatter.cxx\(\"$FILE\"\)
-#root -b -l -q plotXY.cxx\(\"$FILE\"\)
-#root -b -l -q plotXY_neg.cxx\(\"$FILE\"\)
-#root -b -l -q plotXY_pos.cxx\(\"$FILE\"\)
-#root -b -l -q plotPredXY.cxx\(\"$FILE\"\)
 root -b -l -q plotResidualsInitial.C\(\"$FILE\"\)
 root -b -l -q plotResidualsOrderGBLWithErrors.C\(\"$FILE\"\)
 root -b -l -q plotResidualsVSPos_new.C\(\"$FILE\"\)
@@ -17,9 +10,6 @@ root -b -l -q plotResidualsVSPosProfile_new.C\(\"$FILE\"\)
 root -b -l -q plotResidualsUVSPosV_new.C\(\"$FILE\"\)
 root -b -l -q plotResidualsUVSPosVProfile_new.C\(\"$FILE\"\)
 root -b -l -q plotXY.C\(\"$FILE\"\)
-root -b -l -q plotXY_neg.C\(\"$FILE\"\)
-root -b -l -q plotXY_pos.C\(\"$FILE\"\)
-root -b -l -q plotXY_negPos.C\(\"$FILE\"\)
 root -b -l -q plotPredXY.C\(\"$FILE\"\)
 root -b -l -q plotKinkLambda.C\(\"$FILE\"\)
 root -b -l -q plotKinkPhi.C\(\"$FILE\"\)
@@ -33,7 +23,7 @@ root -b -l -q plotSingleSensor.C\(\"5H\",\"$FILE\"\)
 root -b -l -q plotSingleSensor.C\(\"5S\",\"$FILE\"\)
 root -b -l -q plotSingleSensor.C\(\"6H\",\"$FILE\"\)
 root -b -l -q plotSingleSensor.C\(\"6S\",\"$FILE\"\)
-root -b -l -q survey_d0z0p.C\(\"$FILE\"\)
+root -b -l -q survey_d0z0.C\(\"$FILE\"\)
 
 mkdir $TYPE
 mv -fv *__* $TYPE/.
